b6cf7e7d2388cef381a61a56905c88d650958e68
Benjamin Cortes <ben@bencortes.com>
2019-11-20 10:39:13 -0500

    how to run Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cli-tool-prototype
+
+### Run cli tool
+`./cli.js yourArgument`
+
+Files needs to be executable and this needs to happen on the client machine:
+`chmod +x cli.js`
--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const [fist, second, ...args] = process.argv;
 
-console.log(process.argv)
-args.forEach(element => {
-  console.log(element);
-});
+// console.log(process.argv)
+// args.forEach(element => {
+//   console.log(element);
+// });
 console.log(`Good morning ${args.join(' ')}`);
